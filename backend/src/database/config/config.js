9e30488bd452888c2493b3f947ebe478f523cd12
Marllon <marllon3636@gmail.com>
2022-11-16 20:53:19 -0300

    fix: adjust database config

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -1,18 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
     username: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
     host: process.env.HOSTNAME,
-    dialect: 'postgres',
-  },
-  production: {
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: 'postgres',
-  },
+    dialect: 'postgres'
 };
