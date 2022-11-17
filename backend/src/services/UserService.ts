fede7b9e1b69f783c5d60aceedbbf752c6408d9c
Marllon <marllon3636@gmail.com>
2022-11-17 07:25:01 -0300

    feat: add hashed password verification to login service

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -27,8 +27,9 @@ class UserService {
   static async login(obj: ILogin): Promise<Object> {
     const { username, password } = obj;
     const verify = await UserModel.findOne({ where: { username } });
+    const translated = await translateMd5(password);
 
-    if (!verify) throw new Error('Incorrect email or password');
+    if (!verify || verify.password !== translated) throw new Error('Incorrect email or password');
 
     const secret = String(JWT_SECRET);
     const token = sign({ payload: username }, secret, { expiresIn: '24h' });
