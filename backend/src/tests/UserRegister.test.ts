dc21175d707bba153a632a851a18cb5c2e5bfc5f
Marllon <marllon3636@gmail.com>
2022-11-17 16:26:41 -0300

    test: implement user register request tests

---
+++ backend/src/tests/UserRegister.test.ts
@@ -0,0 +1,80 @@
+import chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('[POST] Register', () => {
+  it('Status 201 is returned when register request is successful and a token is returned', async () => {
+    const res = await chai
+      .request(app).post('/register')
+      .send({
+        username: "newtesting",
+        password: "Testing12345",
+      });
+
+    expect(res.status).to.be.equal(201);
+    expect(res.body).to.be.an('Object');
+    expect(res.body).to.have.keys('token');    
+  });
+
+  it('An error message is returned when registering an invalid username', async () => {
+    const res = await chai
+      .request(app).post('/register')
+      .send({
+        username: "er",
+        password: "Error12345",
+      });
+
+    expect(res.status).to.be.equal(400);
+    expect(res.body.message).to.be.equal('Username must be at least three characters long');
+  });
+
+  it('An error message is returned when registering password with less than eight characters', async () => {
+    const res = await chai
+      .request(app).post('/register')
+      .send({
+        username: "error",
+        password: "Error12",
+      });
+
+    expect(res.status).to.be.equal(400);
+    expect(res.body.message).to.be.equal('Password must be at least eight characters long');
+  });
+
+  it('An error message is returned when registering password without an uppercase letter', async () => {
+    const res = await chai
+      .request(app).post('/register')
+      .send({
+        username: "error",
+        password: "error12345",
+      });
+
+    expect(res.status).to.be.equal(400);
+    expect(res.body.message).to.be.equal('Password needs at least one uppercase letter');
+  });
+
+  it('Status 400 is returned when a register field is missing', async () => {
+    const res = await chai
+      .request(app).post('/register')
+      .send({
+        username: "missing",
+      });
+
+    expect(res.status).to.be.equal(400);
+  });
+
+  it('A warning message is returned when a register field is missing', async () => {
+    const res = await chai
+      .request(app).post('/register')
+      .send({
+        username: "missing",
+      });
+
+    expect(res.body.message).to.be.equal('All fields must be filled');
+  });
+});
