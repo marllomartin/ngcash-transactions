ecb608cab4cd3b6eb78a532cc619b64c463dca45
Marllon <marllon3636@gmail.com>
2022-11-17 05:11:49 -0300

    feat: add account and transaction routes to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import UserRoutes from './routes/UserRoutes';
+import AccountRoutes from './routes/AccountRoutes';
+import TransactionRoutes from './routes/TransactionRoutes';
 
 class App {
   public app: express.Express;
@@ -12,7 +14,7 @@ class App {
     this.app.get('/', (req, res) => res.json({ message: 'Ok' }));
   }
 
-  private config():void {
+  private config(): void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
@@ -23,10 +25,10 @@ class App {
     this.app.use(express.json());
     this.app.use(accessControl);
 
-    this.app.use(UserRoutes);
+    this.app.use(UserRoutes, AccountRoutes, TransactionRoutes);
   }
 
-  public start(PORT: string | number):void {
+  public start(PORT: string | number): void {
     this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
   }
 }
