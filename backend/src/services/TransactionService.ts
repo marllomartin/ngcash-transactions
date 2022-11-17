5c9c6bf90d111b5bca8bd08c347946c89e050208
Marllon <marllon3636@gmail.com>
2022-11-17 13:54:10 -0300

    feat: add new verifications to transaction creating service

--- backend/src/services/TransactionService.ts
+++ backend/src/services/TransactionService.ts
@@ -4,21 +4,31 @@ import AccountModel from '../database/models/accounts'
 import TransactionModel from '../database/models/transactions';
 
 class TransactionService {
-  static async createTransaction(obj: ITransaction): Promise<any> {
+  static async createTransaction(obj: ITransaction): Promise<Object> {
     const { debitedAccountId, creditedAccountId, value } = obj;
 
     if (debitedAccountId === creditedAccountId) {
       throw new Error('Invalid transaction');
     }
 
-    const debitAccount = await AccountModel
-      .update({ balance: Sequelize.literal(`balance - ${value}`) }, { where: { id: debitedAccountId } });
-    const creditAccount = await AccountModel
-      .update({ balance: Sequelize.literal(`balance + ${value}`) }, { where: { id: creditedAccountId } });
+    const userAccount = await AccountModel.findOne({ where: { id: debitedAccountId } });
 
-    if (debitAccount && creditAccount) {
-      return await TransactionModel.create({ debitedAccountId, creditedAccountId, value });
+    if (userAccount) {
+      if (userAccount.balance < value) {
+        throw new Error('Not enough funds');
+      }
+
+      const debitAccount = await AccountModel
+        .update({ balance: Sequelize.literal(`balance - ${value}`) }, { where: { id: debitedAccountId } });
+      const creditAccount = await AccountModel
+        .update({ balance: Sequelize.literal(`balance + ${value}`) }, { where: { id: creditedAccountId } });
+
+      if (debitAccount && creditAccount) {
+        return await TransactionModel.create({ debitedAccountId, creditedAccountId, value });
+      }
     }
+
+    throw new Error('Unknown error');
   };
 
   static async getUserTransactions(id: number): Promise<Object> {
