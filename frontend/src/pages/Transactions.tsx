4883ff88e904f53342862944407774bd10c2554a
marllomartin <marllon3636@gmail.com>
2022-11-18 19:26:29 -0300

    feat: create transactions page

---
+++ frontend/src/pages/Transactions.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LayoutTransactions from '../components/LayoutTransactions';
+
+const Login: React.FC = () => (
+  <>
+    <LayoutTransactions />
+  </>
+)
+
+export default Login;
