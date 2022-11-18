94e0cda9799a7876b074654ff2ca5263dbd69ac5
marllomartin <marllon3636@gmail.com>
2022-11-18 19:23:09 -0300

    feat: create login layout component

---
+++ frontend/src/components/LayoutLogin/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from './styles'
+import UserLogin from '../UserLogin';
+
+const LayoutLogin: React.FC = () => (
+  <Container>
+    <UserLogin />
+  </Container>
+)
+
+export default LayoutLogin;
