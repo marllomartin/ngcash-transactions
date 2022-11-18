89f844b3cfd0e63629ba10788662f7416083713b
marllomartin <marllon3636@gmail.com>
2022-11-18 19:24:32 -0300

    feat: create menu styled component

---
+++ frontend/src/components/Menu/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './styles';
+
+const Menu: React.FC = () => (
+  <Container>
+    <nav>
+      <a href="/">Log out</a>
+    </nav>
+  </Container>
+)
+
+export default Menu;
---
+++ frontend/src/components/Menu/styles.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 20px;
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+
+  a {
+    
+    font-size: 20px;
+    margin: 10px;
+    color: #8170F4;
+    padding: 10px;
+    white-space: nowrap;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 600;
+  }
+`
