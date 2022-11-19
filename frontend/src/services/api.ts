c828385686719c9ecb0cf6eaf7f276a368a656f7
marllomartin <marllon3636@gmail.com>
2022-11-19 05:55:18 -0300

    feat: add more services to api axios

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -11,3 +11,26 @@ export const login = async (username: string, password: string) => {
 export const register = async (username: string, password: string) => {
   return api.post("register", { username, password });
 };
+
+export const getTransactions = async (token: any) => {
+  return api.get(
+    "user/transactions", {
+    headers: {
+      "authorization": token
+    }
+  });
+};
+
+export const createTransaction = async (token: any, debitedAccountId: number, creditedAccountUsername: string, value: number) => {
+  return api.post(
+    "transaction",
+    {
+      debitedAccountId,
+      creditedAccountUsername,
+      value
+    }, {
+    headers: {
+      "authorization": token
+    }
+  });
+};
