62a0399cc57ba7ead25ae9954f3c844b97dd7336
marllomartin <marllon3636@gmail.com>
2022-11-19 05:58:13 -0300

    refactor: update transactions create

--- frontend/src/components/TransactionsCreate/index.tsx
+++ frontend/src/components/TransactionsCreate/index.tsx
@@ -1,29 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CurrencyInput from 'react-currency-input-field';
+import { createTransaction } from '../../services/api';
 import { Container } from '../../styles/ContainerStyle';
 import { ButtonArea, Form, InputContainer, InputGroup } from './styles';
 
 const TransactionsCreate: React.FC = () => {
+  const [userData] = useState(JSON.parse(localStorage.getItem('user') || '{}'));
+  const [creditedUser, setCreditedUser] = useState<string>("");
+  const [value, setValue] = useState<any>("");
+
+  const handleChangeCreditedUser = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCreditedUser(event.target.value);
+  }
+
+  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      await createTransaction(userData.token, 5, creditedUser, Number(value))
+    } catch (error) {
+      console.log(error);
+    }
+    setCreditedUser("");
+    setValue(0);
+  }
 
   return (
     <>
       <Container>
         <header>Nova Transferência</header>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <InputContainer>
             <InputGroup>
-              <label htmlFor="name">Usuário a receber:</label>
+              <label htmlFor="name">Transferir para:</label>
               <input
+                id="userCredit"
+                type="text"
+                value={creditedUser}
+                placeholder="nome de usuário"
+                onChange={handleChangeCreditedUser}
               />
             </InputGroup>
             <InputGroup>
               <label htmlFor="description">Valor:</label>
               <input
+                id="value"
                 type="number"
+                value={value}
+                onChange={handleChangeValue}
               />
             </InputGroup>
           </InputContainer>
           <ButtonArea>
-            <button type="submit">Cadastrar</button>
+            <button type="submit">Transferir</button>
           </ButtonArea>
         </Form>
       </Container>
