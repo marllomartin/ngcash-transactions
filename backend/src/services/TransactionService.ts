d2f62c60313f41a31a646912a7282b2b09d4a2ff
marllomartin <marllon3636@gmail.com>
2022-11-19 20:13:42 -0300

    refactor: new logic for transaction service

--- backend/src/services/TransactionService.ts
+++ backend/src/services/TransactionService.ts
@@ -1,22 +1,37 @@
 import { Sequelize, Op } from 'sequelize';
 import ITransaction from '../interfaces/ITransaction';
 import AccountModel from '../database/models/accounts';
+import UserModel from '../database/models/users';
 import TransactionModel from '../database/models/transactions';
 
 class TransactionService {
+  // eslint-disable-next-line max-lines-per-function
   static async createTransaction(obj: ITransaction): Promise<object> {
-    const { debitedAccountId, creditedAccountId, value } = obj;
+    const { debitedAccountId, creditedAccountUsername, value } = obj;
     const userAccount = await AccountModel.findOne({ where: { id: debitedAccountId } });
+
+    const creditedUserId = await UserModel
+      .findOne({ where: { username: creditedAccountUsername } });
+    const creditedAccount = await AccountModel
+      .findOne({ where: { id: creditedUserId?.accountId } });
+
+    const creditedAccountId = creditedAccount?.id;
+
+    if (debitedAccountId === creditedAccountId) throw new Error('Cannot send funds to yourself.');
 
     if (userAccount) {
       if (userAccount.balance < value) throw new Error('Not enough funds');
 
       const debitAccount = await AccountModel
-        .update({ balance: Sequelize
-          .literal(`balance - ${value}`) }, { where: { id: debitedAccountId } });
+        .update({
+          balance: Sequelize
+            .literal(`balance - ${value}`),
+        }, { where: { id: debitedAccountId } });
       const creditAccount = await AccountModel
-        .update({ balance: Sequelize
-          .literal(`balance + ${value}`) }, { where: { id: creditedAccountId } });
+        .update({
+          balance: Sequelize
+            .literal(`balance + ${value}`),
+        }, { where: { id: creditedAccountId } });
 
       if (debitAccount && creditAccount) {
         const transaction = await TransactionModel
@@ -29,10 +44,25 @@ class TransactionService {
   }
 
   static async getUserTransactions(id: number): Promise<object> {
-    const transactions = await TransactionModel
+    const users = await UserModel.findAll();
+    const userTransactions = await TransactionModel
       .findAll({ where: { [Op.or]: [{ debitedAccountId: id }, { creditedAccountId: id }] } });
 
-    return transactions;
+    const result = userTransactions.map(
+      ({ id, debitedAccountId, creditedAccountId, value, createdAt }) => (
+        {
+          id,
+          debitedAccountUsername: users
+            .find((user) => user.dataValues.id === debitedAccountId)?.username,
+          creditedAccountUsername: users
+            .find((user) => user.dataValues.id === creditedAccountId)?.username,
+          value,
+          createdAt,
+        }
+      ),
+    );
+
+    return result;
   }
 }
 
