2e829f561626e60376ef6e39f1c42e924bde1a31
marllomartin <marllon3636@gmail.com>
2022-11-19 05:56:26 -0300

    style: update global styles with background animation and app font

--- frontend/src/styles/GlobalStyle.ts
+++ frontend/src/styles/GlobalStyle.ts
@@ -6,12 +6,27 @@ export default createGlobalStyle`
     padding: 0;
     outline:0;
     box-sizing:border-box;
-    font-family: 'Open Sans', sans-serif; 
+    font-family: 'IBM Plex Sans', sans-serif;
   }
   #root{
     margin:0 auto;
   }
   body {
-    background-color: white;
+	    background: linear-gradient(-45deg, #e2e2e2, #DCDCDC, #EDEDED, #FFFF, #E6E6E6, #DCDCDC, #e2e2e2);
+	  background-size: 400% 400%;
+	  animation: gradient 22s ease infinite;
+	  height: 100vh;
   }
+
+  @keyframes gradient {
+	  0% {
+		  background-position: 0% 50%;
+	  }
+	  50% {
+		  background-position: 100% 50%;
+	  }
+	  100% {
+		background-position: 0% 50%;
+	  }
+}
 `
