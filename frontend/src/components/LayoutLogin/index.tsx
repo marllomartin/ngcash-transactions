00ae98759f9cc1071fd22413ba4b51c10348aacf
marllomartin <marllon3636@gmail.com>
2022-11-19 05:59:30 -0300

    feat: add header to login layout

--- frontend/src/components/LayoutLogin/index.tsx
+++ frontend/src/components/LayoutLogin/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Container } from './styles'
 import UserLogin from '../UserLogin';
+import Header from '../Header';
 
 const LayoutLogin: React.FC = () => (
   <Container>
+    <Header />
     <UserLogin />
   </Container>
 )
